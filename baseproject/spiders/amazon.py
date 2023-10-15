b05993114f3fca058205e9bcedba4c38fa40881f
Reis567 <matheusdosreis9@gmail.com>
2023-10-15 14:53:51 -0300

    30 pages scrap

--- baseproject/spiders/amazon.py
+++ baseproject/spiders/amazon.py
@@ -3,7 +3,15 @@ import scrapy
 
 class AmazonSpider(scrapy.Spider):
     name = "amazon"
-    start_urls = ["https://www.amazon.com.br/s?k=SSD+1TB"]
+    def start_requests(self):
+        base_url = "https://www.amazon.com.br/s?k=SSD+1TB&page={page}&qid=1697392045&ref=sr_pg_2"
+
+        # Defina o número máximo de páginas a serem raspadas
+        max_pages = 30
+
+        for page in range(1, max_pages + 1):
+            url = base_url.format(page=page)
+            yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
         products = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "a-spacing-base", " " ))]')
